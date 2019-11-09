79bb94b51d712b43b9146b4e0af5b01378f77e3c
sakibashrak <officialsakibashrak@gmail.com>
2019-11-09 13:55:57 +0600

    Updates

---
+++ precache-manifest.2feaa28625c70eaec7f9f878874eb4e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9a2a9861f8e6b9f0c3e195b9a6fcf79",
+    "url": "/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "111338c137f22c58f64e",
+    "url": "/react-monsters-rolodex/static/css/main.5bbabe62.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/react-monsters-rolodex/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "111338c137f22c58f64e",
+    "url": "/react-monsters-rolodex/static/js/main.319cb41b.chunk.js"
+  },
+  {
+    "revision": "e0faa840671b71440f1a",
+    "url": "/react-monsters-rolodex/static/js/runtime-main.a4f32280.js"
+  }
+]);
